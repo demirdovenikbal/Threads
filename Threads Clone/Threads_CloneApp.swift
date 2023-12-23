d56e399fdd56d557fb858a49e7e5834735a7dfc9
demirdovenikbal <ikbaldemirdoven@icloud.com>
2023-12-23 14:15:59 -0800

    Initial Commit

---
+++ Threads Clone/Threads_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Threads_CloneApp.swift
+//  Threads Clone
+//
+//  Created by Ikbal Demirdoven on 2023-12-23.
+//
+
+import SwiftUI
+
+@main
+struct Threads_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
